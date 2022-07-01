0791a7d104fd42fc57a1981213557dae9fabbe31
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-07-01 22:29:56 +0600

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;git add <div className=""></div>
 
 app.use(cors());
 app.use(express.json());
